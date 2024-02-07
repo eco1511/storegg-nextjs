23a572d3d9fe0525513a9afa6d74d96400e32ee1
Eko Teguh Santoso <38200904+eco1511@users.noreply.github.com>
2024-02-07 13:34:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['bwa-storegg-serveradmin.up.railway.app'],
+    domains: ['https://bwa-storegg-serveradmin.up.railway.app/'],
   },
 };
